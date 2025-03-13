28352b1f64f2f2e7fb9cc194262304681e8d1ee3
Roc <roc.bohigas@estudiantat.upc.edu>
2025-03-13 13:22:37 +0100

    hola

--- Pregunta1.R
+++ Pregunta1.R
@@ -1,1 +1,9 @@
 #Solucion Pregunta 1
+
+x <- c(0,1) #vector definit
+fx <- c(0.68,0.32)
+
+#tabla de probavilidad
+cbind(x,fx)
+plot(x,fx, pch=16, col="blue")
+lines(x,fx,type="h", col="red") #linia de la grafica
